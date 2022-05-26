d47d5365a32dd5cf5e6d5196390a96babcc0df02
Paweł Kordowski <pawel.kordowski.codeline@gmail.com>
2022-05-27 01:30:12 +0200

    Use TrainingService in subscription

--- app/graphql/subscriptions/trainings.py
+++ app/graphql/subscriptions/trainings.py
@@ -4,7 +4,7 @@ from uuid import UUID
 import strawberry
 from strawberry.types import Info
 
-from app.domain.repositories.training_repository import TrainingRepository
+from app.domain.services.training_service import TrainingService
 from app.graphql.permissions import IsAuthenticated
 from app.graphql.types import Training
 from app.rabbitmq import get_message, get_new_training_queue_name
@@ -13,10 +13,9 @@ from app.rabbitmq import get_message, get_new_training_queue_name
 async def get_new_friends_training(info: Info):
     user_id = info.context["user_id"]
     async for message in get_message(get_new_training_queue_name(user_id)):
-        async with TrainingRepository() as repository:
-            training = await repository.get_training_by_id(
-                request_user_id=user_id, training_id=UUID(message["training_id"])
-            )
+        training = await TrainingService.get_training(
+            request_user_id=user_id, training_id=UUID(message["training_id"])
+        )
         if training:
             yield Training.from_entity(training)
 
