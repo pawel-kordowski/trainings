e72e408ab99c9471b09d24f0967051517506721a
Paweł Kordowski <pawel.kordowski.codeline@gmail.com>
2022-05-27 01:24:05 +0200

    Refactor tests

--- tests/test_api/test_create_training.py
+++ tests/test_api/test_create_training.py
@@ -6,75 +6,69 @@ from app.domain import entities
 from app.jwt_tokens import create_access_token
 
 
-def test_create_training_requires_authorization(client):
-    query = """
-    mutation {
-        createTraining(
-            input:{
-                name: "New training"
-                startTime: "2020-10-10T10:00:00"
-                endTime: "2020-10-10T11:00:00"
-            }
-        ) {
-            id
-        }
-    }
-    """
-    response = client.post("/graphql", json={"query": query})
-
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["data"] is None
-    assert response_json["errors"][0]["message"] == "User is not authenticated"
-
-
 @patch("app.graphql.mutations.trainings.TrainingService", autospec=True)
-def test_create_training_calls_training_service_create_training(
-    mocked_training_service, client
-):
+class TestCreateTraining:
     name = "New training"
     start_time = datetime.fromisoformat("2020-10-10T10:00:00")
     end_time = datetime.fromisoformat("2020-10-10T11:00:00")
-    user_id = uuid4()
-    training_id = uuid4()
-    mocked_training_service.create_training.return_value = entities.Training(
-        id=training_id,
-        name=name,
-        start_time=start_time,
-        end_time=end_time,
-        user_id=user_id,
-    )
-    query = f"""
-    mutation {{
-        createTraining(
-            input:{{
-                name: "{name}"
-                startTime: "{start_time.isoformat()}"
-                endTime: "{end_time.isoformat()}"
+
+    def get_query(self):
+        return f"""
+        mutation {{
+            createTraining(
+                input:{{
+                    name: "{self.name}"
+                    startTime: "{self.start_time.isoformat()}"
+                    endTime: "{self.end_time.isoformat()}"
+                }}
+            ) {{
+                id
+                name
+                startTime
+                endTime
             }}
-        ) {{
-            id
-            name
-            startTime
-            endTime
         }}
-    }}
-    """
-    response = client.post(
-        "/graphql",
-        json={"query": query},
-        headers={"Authorization": f"Bearer {create_access_token(user_id)}"},
-    )
+        """
 
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["data"]["createTraining"] == {
-        "id": str(training_id),
-        "name": name,
-        "startTime": start_time.isoformat(),
-        "endTime": end_time.isoformat(),
-    }
+    def test_requires_authorization(self, mocked_training_service, client):
+        response = client.post("/graphql", json={"query": self.get_query()})
 
-    mocked_training_service.create_training.assert_awaited_once_with(
-        user_id=user_id, name=name, start_time=start_time, end_time=end_time
-    )
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["data"] is None
+        assert response_json["errors"][0]["message"] == "User is not authenticated"
+
+    def test_calls_training_service_create_training(
+        self, mocked_training_service, client
+    ):
+        user_id = uuid4()
+        training = entities.Training(
+            id=uuid4(),
+            name=self.name,
+            start_time=self.start_time,
+            end_time=self.end_time,
+            user_id=user_id,
+        )
+        mocked_training_service.create_training.return_value = training
+
+        response = client.post(
+            "/graphql",
+            json={"query": self.get_query()},
+            headers={"Authorization": f"Bearer {create_access_token(user_id)}"},
+        )
+
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["data"]["createTraining"] == {
+            "id": str(training.id),
+            "name": training.name,
+            "startTime": training.start_time.isoformat(),
+            "endTime": training.end_time.isoformat(),
+        }
+
+        mocked_training_service.create_training.assert_awaited_once_with(
+            user_id=user_id,
+            name=self.name,
+            start_time=self.start_time,
+            end_time=self.end_time,
+        )
--- tests/test_api/test_create_user.py
+++ tests/test_api/test_create_user.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from uuid import uuid4
 
 from app.domain import entities
@@ -7,153 +7,116 @@ from app.graphql.exceptions import InvalidPassword, InvalidEmail
 
 
 @patch("app.graphql.input_types.validate_email")
-@patch("app.graphql.input_types.validate_password", MagicMock())
-def test_create_user_invalid_email(mocked_validate_email, client):
-    mocked_validate_email.side_effect = InvalidEmail()
-    email = "email"
-    password = "password"
-    query = f"""
-    mutation {{
-        createUser(
-            input:{{
-                email: "{email}"
-                password: "{password}"
-            }}
-        ) {{
-            __typename
-            ... on User{{
-                id
-                email
-            }}
-            ... on Error{{
-                message
-            }}
-        }}
-    }}
-    """
-    response = client.post("/graphql", json={"query": query})
-
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["data"]["createUser"] == {
-        "__typename": "Error",
-        "message": InvalidEmail.message,
-    }
-
-
-@patch("app.graphql.input_types.validate_email", MagicMock())
 @patch("app.graphql.input_types.validate_password")
-def test_create_user_invalid_password(mocked_validate_password, client):
-    error_message = "Error message"
-    mocked_validate_password.side_effect = InvalidPassword(message=error_message)
+@patch("app.graphql.mutations.users.UserService", autospec=True)
+class TestCreateUser:
     email = "email"
     password = "password"
-    query = f"""
-    mutation {{
-        createUser(
-            input:{{
-                email: "{email}"
-                password: "{password}"
-            }}
-        ) {{
-            __typename
-            ... on User{{
-                id
-                email
-            }}
-            ... on Error{{
-                message
+
+    def get_query(self):
+        return f"""
+        mutation {{
+            createUser(
+                input:{{
+                    email: "{self.email}"
+                    password: "{self.password}"
+                }}
+            ) {{
+                __typename
+                ... on User{{
+                    id
+                    email
+                }}
+                ... on Error{{
+                    message
+                }}
             }}
         }}
-    }}
-    """
-    response = client.post("/graphql", json={"query": query})
+        """
 
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["data"]["createUser"] == {
-        "__typename": "Error",
-        "message": error_message,
-    }
+    def test_invalid_email(
+        self,
+        mocked_user_service,
+        mocked_validate_password,
+        mocked_validate_email,
+        client,
+    ):
+        mocked_validate_email.side_effect = InvalidEmail()
 
+        response = client.post("/graphql", json={"query": self.get_query()})
 
-@patch("app.graphql.input_types.validate_email", MagicMock())
-@patch("app.graphql.input_types.validate_password", MagicMock())
-@patch("app.graphql.mutations.users.UserService", autospec=True)
-def test_create_user_new_email(mocked_user_service, client):
-    email = "test@test.com"
-    password = "passwordA1!"
-    user_id = uuid4()
-    mocked_user_service.create_user.return_value = entities.User(
-        id=user_id, email=email
-    )
-    query = f"""
-    mutation {{
-        createUser(
-            input:{{
-                email: "{email}"
-                password: "{password}"
-            }}
-        ) {{
-            __typename
-            ... on User{{
-                id
-                email
-            }}
-            ... on Error{{
-                message
-            }}
-        }}
-    }}
-    """
-    response = client.post("/graphql", json={"query": query})
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["data"]["createUser"] == {
+            "__typename": "Error",
+            "message": InvalidEmail.message,
+        }
+        mocked_validate_email.assert_called_once_with(self.email)
 
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["data"]["createUser"] == {
-        "__typename": "User",
-        "id": str(user_id),
-        "email": email,
-    }
-    mocked_user_service.create_user.assert_awaited_once_with(
-        email=email, password=password
-    )
+    def test_invalid_password(
+        self,
+        mocked_user_service,
+        mocked_validate_password,
+        mocked_validate_email,
+        client,
+    ):
+        error_message = "Error message"
+        mocked_validate_password.side_effect = InvalidPassword(message=error_message)
 
+        response = client.post("/graphql", json={"query": self.get_query()})
 
-@patch("app.graphql.input_types.validate_email", MagicMock())
-@patch("app.graphql.input_types.validate_password", MagicMock())
-@patch("app.graphql.mutations.users.UserService", autospec=True)
-def test_create_user_existing_email(mocked_user_service, client):
-    mocked_user_service.create_user.side_effect = EmailAlreadyExists()
-    email = "test@test.com"
-    password = "passwordA1!"
-    query = f"""
-    mutation {{
-        createUser(
-            input:{{
-                email: "{email}"
-                password: "{password}"
-            }}
-        ) {{
-            __typename
-            ... on User{{
-                id
-                email
-            }}
-            ... on Error{{
-                message
-            }}
-        }}
-    }}
-    """
-    response = client.post("/graphql", json={"query": query})
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["data"]["createUser"] == {
+            "__typename": "Error",
+            "message": error_message,
+        }
+        mocked_validate_password.assert_called_once_with(self.password)
 
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["data"]["createUser"] == {
-        "__typename": "Error",
-        "message": "Email address already taken",
-    }
-    mocked_user_service.create_user.assert_awaited_once_with(
-        email=email, password=password
-    )
+    def test_new_email(
+        self,
+        mocked_user_service,
+        mocked_validate_password,
+        mocked_validate_email,
+        client,
+    ):
+        user = entities.User(id=uuid4(), email=self.email)
+        mocked_user_service.create_user.return_value = user
+
+        response = client.post("/graphql", json={"query": self.get_query()})
+
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["data"]["createUser"] == {
+            "__typename": "User",
+            "id": str(user.id),
+            "email": user.email,
+        }
+        mocked_validate_password.assert_called_once_with(self.password)
+        mocked_validate_email.assert_called_once_with(self.email)
+        mocked_user_service.create_user.assert_awaited_once_with(
+            email=self.email, password=self.password
+        )
+
+    def test_existing_email(
+        self,
+        mocked_user_service,
+        mocked_validate_password,
+        mocked_validate_email,
+        client,
+    ):
+        mocked_user_service.create_user.side_effect = EmailAlreadyExists()
+
+        response = client.post("/graphql", json={"query": self.get_query()})
+
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["data"]["createUser"] == {
+            "__typename": "Error",
+            "message": "Email address already taken",
+        }
+        mocked_validate_password.assert_called_once_with(self.password)
+        mocked_validate_email.assert_called_once_with(self.email)
+        mocked_user_service.create_user.assert_awaited_once_with(
+            email=self.email, password=self.password
+        )
