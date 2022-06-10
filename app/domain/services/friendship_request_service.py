fb79773082ab935e59a71778892e34584dd12c57
Paweł Kordowski <pawel.kordowski.codeline@gmail.com>
2022-06-10 19:52:44 +0200

    Small refactor

--- app/domain/services/friendship_request_service.py
+++ app/domain/services/friendship_request_service.py
@@ -18,15 +18,17 @@ class FriendshipRequestService:
     async def create_friendship_request(
         sender_id: UUID, receiver_id: UUID
     ) -> entities.FriendshipRequest:
-        async with UserRepository() as user_repository:
+        async with (
+            UserRepository() as user_repository,
+            FriendshipRepository() as friendship_repository,
+            FriendshipRequestRepository() as friendship_request_repository,
+        ):
             if not await user_repository.does_user_with_id_exist(user_id=receiver_id):
                 raise ReceiverDoesNotExist
-        async with FriendshipRepository() as friendship_repository:
             if await friendship_repository.are_users_friends(
                 user_1_id=sender_id, user_2_id=receiver_id
             ):
                 raise UsersAreAlreadyFriends
-        async with FriendshipRequestRepository() as friendship_request_repository:
             if await friendship_request_repository.does_pending_request_exists(
                 user_1_id=sender_id, user_2_id=receiver_id
             ):
